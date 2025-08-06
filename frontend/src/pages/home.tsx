5aa7b53bf855ed106571c5f510b3df5087b6df0c
raguladhithyam <mra20031006@gmail.com>
2025-08-07 01:04:34 +0530

    basic local env setup

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -69,7 +69,7 @@ export function HomePage() {
       {/* Hero Section */}
       <section className="relative overflow-hidden bg-gradient-to-br from-yugam-50 via-white to-yugam-100 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 py-32 sm:py-40">
         {/* Animated Background */}
-        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23f59532" fill-opacity="0.1"%3E%3Ccircle cx="30" cy="30" r="4"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-40" />
+        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=\'60\' height=\'60\' viewBox=\'0 0 60 60\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cg fill=\'none\' fill-rule=\'evenodd\'%3E%3Cg fill=\'%23f59532\' fill-opacity=\'0.1\'%3E%3Ccircle cx=\'30\' cy=\'30\' r=\'4\'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-40" />
         
         {/* Floating Elements */}
         <div className="absolute top-20 left-10 w-20 h-20 bg-yugam-200 rounded-full opacity-20 animate-bounce" style={{ animationDelay: '0s', animationDuration: '3s' }} />
